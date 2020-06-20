568466b1e7c72afdf518c79df8a9a7ec42a79d6a
Akshat2205 <akshatpal1232@gmail.com>
2020-06-20 23:02:26 +0530

    custom characters added

--- bounce/bounce.ino
+++ bounce/bounce.ino
@@ -3,51 +3,64 @@
 LiquidCrystal lcd(3,2,4,5,6,7);
 const int button = 8;
 char ascii = 0x00+0;
+byte boy[8]={B01110,B10001,B10001,B01110,B00100,B11111,B00100,B11011};
+byte life[8]={B00000,B00000,B11011,B11111,B01110,B00100,B00000,B00000};
+byte block[8]={B11111,B11111,B11111,B11011,B11111,B11111,B11111,B11111};
+byte ball[8]={B00000,B00000,B01110,B11111,B11111,B01110,B000000};
 
-byte customchar[8]={B00000,B01010,B00000,B00000,B00000,B00000,B11111,};
-byte a[8]={B00000,B01010,B00100,B00100,B00000,B01110,B10001,};
-byte s[8]={B00100,B01010,B10001,B10001,B01010,B00100,};
-byte s1[8]={B01110,B01010,B11111,B11011,B11111,B01010,B01110,};
-byte s2[8]={B01010,B00100,B00100,B01010,B10001,B00100,B10001,};
-byte s3[8]={B00100,B01010,B11111,B01010,B10101,B11011,B10001,};
-byte s4[8]={0x1F,0x11,0x11,0x11,0x11,0x11,0x1F,};
-byte s5[8]={B11111,B11101,B11011,B11101,B11111,B10000,B10000,B10000,};
-
-void setup() {
-	lcd.begin(16,2);
+void setup()
+{
   Serial.begin(9600);
   pinMode(button, INPUT);
+  lcd.begin(16 ,2);  //Initialize 16x2 lcd
+  lcd.clear();       //Clear lcd display screen
+  lcd.createChar(1, boy);
+  lcd.createChar(2, life);
+  lcd.createChar(3, block);
+  lcd.createChar(4, ball);
+
+  test();
+  driver();
 }
 
-int get_button_state(){
-  int button_state = digitalRead(button);
-  Serial.println(button_state);
-
-  lcd.createChar(1 , customchar);   //Creating custom characters in CG-RAM
-  lcd.createChar(2 , a);                              
-  lcd.createChar(3 , s);
-  lcd.createChar(4 , s1);
-  lcd.createChar(5 , s2);
-  lcd.createChar(6 , s3);
-  lcd.createChar(7 , s4);
-  lcd.createChar(8 , s5); //Creating custom characters in CG-RAM
-}
-
-void loop() {
-//  lcd.setCursor(3,1);
-//  lcd.print(ascii);
-//  delay(500);
-//  ascii++;
-    int rand,i;
-  lcd.setCursor(0 ,0);            //Place lcd cursor on first row and first coulomb of 16x2 lcd
-  lcd.print("Cust Character!!");  //Display this test on first row on 16x2 lcd
-  lcd.setCursor(0 ,1);            //Place lcd cursor on second row of 16x2 lcd first coulomb
-
-  for(rand=0;rand<10;rand++){     //For loop will display custom characters one by one
-    i=random(10);
-    lcd.setCursor(i ,1);
-    lcd.write(i);                 //-->>>PRINTING/DISPLAYING CUSTOM CHARACTERS
-    delay(500);
+void test(){
+  for(int i=0; i<=15; i++) {
+    lcd.setCursor(i,0);
+    lcd.write(3);
   }
-  lcd.clear();              
+  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.clear();
+}
+
+void driver(){
+  int init_value = digitalRead(button);
+  bool state = false;
+  int chance = 13;
+  for(int i=chance; i<=16; ++i){
+    lcd.setCursor(i,0);
+    lcd.write(2);
+  }
+  delay(500);
+
+  if(init_value == 1){
+    state = true;
+    if(state){
+      lcd.clear();
+      lcd.setCursor(1,0);
+      lcd.write(1);
+    }
+  }
+  else if(init_value == 0){
+    state = true;
+    if(state){
+      lcd.clear();
+      lcd.setCursor(1,1);
+      lcd.write(1);
+    }
+  }
+  delay(100);
+}
+
+void loop(){ 
 }
